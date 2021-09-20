bf56f43d2dd372f945e2c4903788ce724b352e54
anthony <anthony@poons.us>
2021-09-20 10:12:55 -0400

    addede tests to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
 #include <iostream>
+#include "lab3.h"
+int main()
+{
+  std::cout<<box(3,4)<<"\n";
+  std::cout<<checkerboard(10,5)<<"\n";
+  std::cout<<cross(10)<<"\n";
+  std::cout<<cross(9)<<"\n";
+}
